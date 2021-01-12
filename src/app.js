55afee251cf8ae64e46a3ed341762482e69e3d85
jayparmar271 <jayparmar271@gmail.com>
2021-01-13 01:57:43 +0530

    Create component in react

--- src/app.js
+++ src/app.js
@@ -1,60 +1,29 @@
-console.log('Hello world!');
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of computer',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = '';
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of computer</h2>
+            </div>
+        );
     }
+}
 
-    render();
-};
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
+}
 
-const onRemoveAll = () => {
-    app.options = [];
-    render();
-}; 
+const jsx = (
+    <div>
+        <Header/>
+        <Action/>
+    </div>
+);
 
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-};
-
-const appRoot = document.getElementById("app");
-
-const render = () => {
-    const template = (
-      <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-        <button disabled={app.options.length === 0} onClick={onMakeDecision}>
-          What should I do?
-        </button>
-        <button onClick={onRemoveAll}>Remove All</button>
-        <ol>
-          {app.options.map((option) => (
-            <li key={option}>{option}</li>
-          ))}
-        </ol>
-        <form onSubmit={onFormSubmit}>
-          <input type="text" name="option" />
-          <button>Add Option</button>
-        </form>
-      </div>
-    );
-
-    ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(jsx, document.getElementById('app'));
